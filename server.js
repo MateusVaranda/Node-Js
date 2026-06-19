cbe9cba6980ba02a0b23b0fc1b033dc3c72c6d55
Mateus Varanda Ferreira de Carvalho <mateus.varanda.ferreira@gmail.com>
2026-06-18 20:42:06 -0400

    entendendo CRUD

--- server.js
+++ server.js
@@ -1,5 +1,50 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+import { PrismaClient } from "./generated/prisma/client.js";
 
+const app = express();
+
+app.use(express.json());
+
+const prisma = new PrismaClient();
+
+app.get('/usuarios', async (req, res) => {
+    const users= await prisma.user.findMany()
+
+    res.status(200).json(users)
+})
+
+app.post('/usuarios', async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(user)
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+    const user = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(user)
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({ message: "usuário deletado com sucesso"})
+})
 
 app.listen(3000)
